7c85eba03b13794514ae11c83fd52bbcb5a4034d
Chris <ignatz@gmail.com>
2016-05-23 18:55:44 -0400

    added unittest

---
+++ test_word_pop.py
@@ -0,0 +1,21 @@
+import unittest
+import wordpop as wp
+
+
+class TestWordPop(unittest.TestCase):
+
+    def test_wordpop(self):
+        filename = 'test.txt'
+        f = open(filename, 'r')
+        words = wp.parse(f)
+
+        count = wp.build_dataset(words)
+
+        print('Most common words', count[:10])
+        self.assertEqual(count[0], ('the', 3))
+        self.assertEqual(count[1], ('a', 2))
+        self.assertEqual(count[2], ('quick', 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
