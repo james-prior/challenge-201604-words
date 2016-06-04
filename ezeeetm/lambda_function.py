e55bc972dee2a40cab6f33826677dfd2d7b62f35
James Prior <jep200404@columbus.rr.com>
2016-06-03 23:50:10 -0400

    ezeeetm/lambda_function.py: PEP8
    Old code mixed spaces and tabs for indentation.
    
    Expanded tabs to four spaces.
    Removed spaces inside parentheses.
    Removed spaces between function names and left parenthesis.
    Removed parentheses around value in return statements.
        This make you look like someone who did C before Python.
    Wrapped long lines (except in doctest docstring).
    Specified exception in
    Converted function names from FooBar style to foo_bar style.
    Putting parenthese around a single item is not enough to make a tuple out of
    it. To make a tuple out of a single item, put a comma after the the item.
    For example, (i) is not a tuple. (i,) is a tuple.
    Removed superfluous parentheses.

--- ezeeetm/lambda_function.py
+++ ezeeetm/lambda_function.py
@@ -8,93 +8,100 @@ import re
 import boto3
 import json
 
-def UrlFactory ( id ):
+def url_factory(id):
     try:
         if id <= 9:
             path = '/0'
         else:
             path = ""
-            idArray = [int(i) for i in str(id)]
-            del idArray[-1]
-            for i in idArray:
-                path += "/%s" % ( i )
-        url = "http://www.gutenberg.lib.md.us%s/%s/%s.zip" % ( path, id, id )
-    except:
-        raise Exception ('BAD ID, PLEASE TRY AN INTEGER VALUE FOR ID...')
-	print ( url )
+            id_array = [int(i) for i in str(id)]
+            del id_array[-1]
+            for i in id_array:
+                path += "/%s" % i
+        url = "http://www.gutenberg.lib.md.us%s/%s/%s.zip" % (path, id, id)
+    except ValueError:
+        raise Exception('BAD ID, PLEASE TRY AN INTEGER VALUE FOR ID...')
+    print(url)
     return url
 
-def GetBookTxtZip ( zipUrl, localZip ):
-    resp = requests.get( zipUrl, stream=True )
-    with open( localZip, 'wb' ) as f:
+def get_book_txt_zip(zip_url, local_zip):
+    resp = requests.get(zip_url, stream=True)
+    with open(local_zip, 'wb') as f:
         for chunk in resp.iter_content(chunk_size=1024):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
 
-def WordListFactory ( localZip, id ):
-    fileName = "%s.txt" % ( id )
-    zf = zipfile.ZipFile(localZip)
-    wordList = zf.read(fileName)
-    wordList = wordList.replace('\r\n',' ').replace('\"','').split(' ')
-    wordList = filter(None, wordList)
-    wordList = [word.lower() for word in wordList]
-    wordList = [''.join(c for c in word if c not in string.punctuation) for word in wordList]
-    return wordList
+def word_list_factory(local_zip, id):
+    file_name = "%s.txt" % id
+    zf = zipfile.ZipFile(local_zip)
+    word_list = zf.read(file_name)
+    word_list = word_list.replace('\r\n',' ').replace('\"','').split(' ')
+    word_list = filter(None, word_list)
+    word_list = [word.lower() for word in word_list]
+    word_list = [
+        ''.join(c for c in word if c not in string.punctuation)
+        for word in word_list]
+    return word_list
 
-def WordListToFreqDict ( wordlist ):
+def word_list_to_freq_dict(wordlist):
     wordfreq = [wordlist.count(p) for p in wordlist]
     return dict(zip(wordlist,wordfreq))
 
-def SortFreqDict ( freqdict ):
+def sort_freq_dict(freqdict):
     aux = [(freqdict[key], key) for key in freqdict]
     aux.sort()
     aux.reverse()
     return aux[:10]
 
-def Test ( results ):
-    """This function tests that words in result sets satisfy the definition in README.md
+def test(results):
+    """This function tests that words in result sets satisfy the
+    definition in README.md
 
-    >>> Test('word')
+    >>> test('word')
     True
-    >>> Test('This-Word')
+    >>> test('This-Word')
     if you got word problems I feel bad for you son. I got 99 problems, but " This-Word " aint one: Exception
-    Traceback (most recent call last):
+    Traceback(most recent call last):
     File "/var/task/lambda_function.py", line 73, in lambda_handler
-    Test ( wordFreqListSorted ) File "/var/task/lambda_function.py", line 57, in Test
-    raise Exception (exception)
+    test(word_freq_list_sorted) File "/var/task/lambda_function.py", line 57, in test
+    raise Exception(exception)
     Exception: if you got word problems I feel bad for you son. I got 99 problems, but " This-Word " aint one
     """
-    for resultSet in results:
-        word = resultSet[1]
-        matchObj = re.match( r'\b[a-z]+\b', word)
-        if matchObj is None:
-            exception = "if you got word problems I feel bad for you son.  I got 99 problems, but \" %s \" aint one" % ( word )
-            raise Exception (exception)
+
+    for result_set in results:
+        word = result_set[1]
+        match_obj = re.match(r'\b[a-z]+\b', word)
+        if not match_obj:
+            exception = (
+                "if you got word problems I feel bad for you son.  "
+                "I got 99 problems, but \" %s \" aint one" % word
+            )
+            raise Exception(exception)
     return True
 
-def UploadToS3 ( id, wordFreqList ):
+def upload_to_s3(id, word_freq_list):
     s3 = boto3.resource('s3')
     bucket = 'gutenberg-out'
-    key = "%s.json" % ( id )
-    body = json.dumps(wordFreqList)
+    key = "%s.json" % id
+    body = json.dumps(word_freq_list)
     s3.Object(bucket, key).put(Body=body)
 
-def CleanUp ( localZip ):
-    os.remove(localZip)
+def clean_up(local_zip):
+    os.remove(local_zip)
 
-def lambda_handler ( event, context ):
-	id = event['id']
-	localZip = "/tmp/%s.zip" % ( id )
-	print(id)
-	print(localZip)
-	
-	zipUrl = UrlFactory ( id )
-	GetBookTxtZip ( zipUrl, localZip )
-	wordList = WordListFactory ( localZip, id )
-	wordFreqList = WordListToFreqDict( wordList )
-	wordFreqListSorted = SortFreqDict ( wordFreqList )
-	Test ( wordFreqListSorted )
-	UploadToS3 (id, wordFreqList )
-	
-	print wordFreqListSorted
-	CleanUp ( localZip )
+def lambda_handler(event, context):
+    id = event['id']
+    local_zip = "/tmp/%s.zip" % id
+    print(id)
+    print(local_zip)
+    
+    zip_url = url_factory(id)
+    get_book_txt_zip(zip_url, local_zip)
+    word_list = word_list_factory(local_zip, id)
+    word_freq_list = word_list_to_freq_dict(word_list)
+    word_freq_list_sorted = sort_freq_dict(word_freq_list)
+    test(word_freq_list_sorted)
+    upload_to_s3(id, word_freq_list)
+    
+    print(word_freq_list_sorted)
+    clean_up()
